8bb42e5a834afe5dc44ed6a5f0ed7ac144624c59
kaspeo <119288675+kaspeo@users.noreply.github.com>
2025-05-27 18:59:38 +0200

    Level_1
    First concept of level_1

--- mathquest/Scene/script/level_1.gd
+++ mathquest/Scene/script/level_1.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
 @onready var start: AcceptDialog = $Dialogi/Start
+@onready var misjashape: Area3D = $Door1/Misja
+@onready var misja: AcceptDialog = $Dialogi/Misja
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	show_start_dialog()
 
@@ -13,3 +14,16 @@ func show_start_dialog() -> void:
 
 func _on_start_confirmed() -> void:
 	start.hide()
+	
+func show_misja_dialog() -> void:
+	misja.popup_centered()
+	misja.grab_focus()
+	misja.connect("confirmed", Callable(self, "_on_misja_confirmed"))
+
+func _on_misja_confirmed() -> void:
+	misja.hide()
+	
+func _on_misja_body_entered(body: Node3D) -> void:
+	show_misja_dialog()
+	if Global.get_ui():
+		Global.get_ui().ustaw_misje("Przenieś skrzynki", false)
