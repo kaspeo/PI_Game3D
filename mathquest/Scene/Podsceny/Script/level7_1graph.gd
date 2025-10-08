0e477f1bc136d0758b7d850fe18249680702c199
kaspeo <119288675+kaspeo@users.noreply.github.com>
2025-10-09 01:43:03 +0200

    Level_7 and presentation

--- mathquest/Scene/Podsceny/Script/level7_1graph.gd
+++ mathquest/Scene/Podsceny/Script/level7_1graph.gd
@@ -3,25 +3,26 @@ extends Panel
 var a: float = 0
 var b: float = 0
 var x: float = 0
-var f: Callable = Callable()  # pusty callable, nie null
+var f: Callable = Callable()  # pusty callable
+
+@export var scale_x: float = 40.0
+@export var scale_y: float = 40.0
 
 func _draw():
 	if not f.is_valid():
-		return  # nic nie rysujemy dopóki f nie jest ustawione
+		return
 
-	var scale_x := 40.0
-	var scale_y := 40.0
-	var offset := Vector2(size.x/2, size.y/2)
+	var offset := size / 2
 
 	# osie
 	draw_line(offset + Vector2(-size.x/2,0), offset + Vector2(size.x/2,0), Color.WHITE, 1)
 	draw_line(offset + Vector2(0,-size.y/2), offset + Vector2(0,size.y/2), Color.WHITE,1)
 
 	# wykres funkcji
-	var prev_point = offset + Vector2(-size.x/2, -f.call(-size.x/(2*scale_x))*scale_y)
+	var prev_point = offset + Vector2(-size.x/2, -f.call(-size.x/(2*scale_x)) * scale_y)
 	for i in range(-int(size.x/2), int(size.x/2)):
-		var xx = i/scale_x
-		var yy = -f.call(xx)*scale_y
+		var xx = i / scale_x
+		var yy = -f.call(xx) * scale_y
 		var point = offset + Vector2(i, yy)
 		draw_line(prev_point, point, Color.YELLOW, 2)
 		prev_point = point
