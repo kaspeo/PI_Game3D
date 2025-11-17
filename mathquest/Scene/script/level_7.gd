16c4b2f0b8a4f325f699cc17641ecdc490702388
kaspeo <119288675+kaspeo@users.noreply.github.com>
2025-11-17 04:09:24 +0100

    Repair level_2 and level_7

--- mathquest/Scene/script/level_7.gd
+++ mathquest/Scene/script/level_7.gd
@@ -15,11 +15,11 @@ func show_misja_dialog() -> void:
 	misja.grab_focus()
 	misja.connect("confirmed", Callable(self, "_on_misja_confirmed"))
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
+
+func _on_misja_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		if Global.get_ui():
-			Global.get_ui().ustaw_misje("Uzupelnij wzor i podaje dane", false)
+			Global.get_ui().ustaw_misje("Napraw dwa komputery", false)
 		if not popup_shown:
 			popup_shown = true
 			show_misja_dialog()
-	
